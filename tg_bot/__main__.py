4577312a2394c85e084001c178f2bc2de15fb5d4
F36 <70635838+F36@users.noreply.github.com>
2020-09-06 22:51:14 -0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -21,9 +21,9 @@ PM_START_TEXT = """
 
 *Hello* {}, *My Name is* *{}*! 
 
-*This Bot is exclusively made for @moviesonlydiscussion ; мσνιєѕσиℓу™ Team!*
+*This Bot is exclusively made for @AnimeChatOfficial,*
 
-*Channel:© @moviesonlyhere*
+*Channel:© @AnimeIndexOfficial*
 
 /help for more details..
 """
@@ -33,11 +33,7 @@ Hello Again,
 
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nഈ പറഞ്ഞിരിക്കുന്ന commandകൾ എല്ലാം  / അല്ലെങ്കിൽ ! വെച്ച് ഉപയോഗിക്കാവുന്നതാണ്...\n")
 
-DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/sonoflars) to get me to where I am now, and every donation helps \
-motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+DONATE_STRING = """No Dont."""
 
 IMPORTED = {}
 MIGRATEABLE = []
